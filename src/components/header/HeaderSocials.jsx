cda8fafcae9cf258e3d7d67ca2595df0238d5d3b
SurrenderAt20 <54646142+SurrenderAt20@users.noreply.github.com>
2022-08-23 18:43:55 +0200

    updated social links

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -7,8 +7,8 @@ export const HeaderSocials = () => {
   return (
 
     <div className='header__socials'>
-        <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a>
-        <a href="https://github.com" target="_blank"><FaGithub/></a>
+        <a href="https://www.linkedin.com/in/lasse-vestergaard-fuglsbjerg-296876195/" target="_blank"><BsLinkedin/></a>
+        <a href="https://github.com/SurrenderAt20" target="_blank"><FaGithub/></a>
         <a href="https://dribbble.com" target="_blank"><FiDribbble/></a>
     </div>
     
